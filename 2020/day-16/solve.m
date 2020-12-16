ac6f9674bb4481bc6696f02ddbd677ef01fc315b
Benoit Charles <bcharles@opentrust.com>
2020-12-16 13:18:31 +0100

    Better version for day-16

--- 2020/day-16/solve.m
+++ 2020/day-16/solve.m
@@ -1,4 +1,6 @@
 
+clear variables
+
 [ f, F, T, N ] = readin( 'data.in' );
 [ nt, nf ] = size(N);
 
@@ -9,13 +11,7 @@ disp( sum( N( all( ~V, 3 ) ) ) );
 
 %%
 
-R = permute( all( V(all(any(V,3),2),:,:), 1 ), [3,2,1] ) ;
-
-O = zeros( 1, nf );
-while any( O == 0 )
-  M = R & repmat( (O==0)', 1, nf );
-  i = find( sum(M,1) == 1, 1 );
-  O(M(:,i)) = i;
-end
+[ K, S ] = sort( (1:nf) * permute( all( V(all(any(V,3),2),:,:), 1 ), [3,2,1] ) );
+O( diff( [ 0 K ] ) ) = S;
 
 disp( uint64( prod( T( O( startsWith( fieldnames(f), 'departure' ) ) ) ) ) );
