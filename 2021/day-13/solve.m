36101263ec752487ec8b9ce95e2d4dd61d236131
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2021-12-13 10:35:14 +0100

    Simplifications

--- 2021/day-13/solve.m
+++ 2021/day-13/solve.m
@@ -1,42 +1,28 @@
 
 [ M, F ] = readin( 'input.txt' );
 
-M = fold( M, F(1) );
+M = fold( M, F(1).axe, F(1).val );
 sum( M(:) )
 
-%%²
+%%
 
 for f = F(2:end)
-  M = fold( M, f );
+  M = fold( M, f.axe, f.val );
 end
 
-pcolor( [ ~M; true(1,size(M,2)) ] )
+pcolor( [ ~M; false(1,size(M,2)) ] )
 colormap( gray(2) )
 axis ij
 axis equal
 
 %%
 
-function M = fold( M, f )
-
-sm = size( M );
-
-if f.axe == 'x'
-
-  P = M(:,end:-1:f.val+1);
-  M(:,f.val:end) = [];
-  Mpad = false( sm(1), max( sm(2) - 2*f.val + 1, 0 ) );
-  Ppad = false( sm(1), max( 2*f.val - sm(2) - 1, 0 ) );
-  M = [ Mpad M ] | [ Ppad P ];
-
-else
-
-  P = M(end:-1:f.val+1,:);
-  M(f.val:end,:) = [];
-  Mpad = false( max( sm(1) - 2*f.val + 1, 0 ), sm(2) );
-  Ppad = false( max( 2*f.val - sm(1) - 1, 0 ), sm(2) );
-  M = [ Mpad ; M ] | [ Ppad ; P ];
-
-end
-
+function M = fold( M, axe, val )
+  if axe == 'y'
+    M = fold( M', 'x', val )';
+  else
+    P = M(:,end:-1:val+1);
+    M(:,val:end) = [];
+    M = M | P;
+  end
 end
