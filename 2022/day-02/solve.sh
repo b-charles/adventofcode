793a10eea4b880bda9953f58ac4986a4f686692d
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2022-12-02 11:51:20 +0100

    Don't use bc

--- 2022/day-02/solve.sh
+++ 2022/day-02/solve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat input.txt | sed 's/A X/4/;s/A Y/8/;s/A Z/3/;s/B X/1/;s/B Y/5/;s/B Z/9/;s/C X/7/;s/C Y/2/;s/C Z/6/' | sed -z 's/\n/+/g;s/+$/\n/' | bc
+echo $((`cat input.txt | sed 's/A X/4/;s/A Y/8/;s/A Z/3/;s/B X/1/;s/B Y/5/;s/B Z/9/;s/C X/7/;s/C Y/2/;s/C Z/6/' | sed -z 's/\n/+/g;s/+$/\n/'`))
 
-cat input.txt | sed 's/A X/3/;s/A Y/4/;s/A Z/8/;s/B X/1/;s/B Y/5/;s/B Z/9/;s/C X/2/;s/C Y/6/;s/C Z/7/' | sed -z 's/\n/+/g;s/+$/\n/' | bc
+echo $((`cat input.txt | sed 's/A X/3/;s/A Y/4/;s/A Z/8/;s/B X/1/;s/B Y/5/;s/B Z/9/;s/C X/2/;s/C Y/6/;s/C Z/7/' | sed -z 's/\n/+/g;s/+$/\n/'`))
 
