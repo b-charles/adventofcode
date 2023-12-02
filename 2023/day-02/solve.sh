269f7733c476d143497b9f68ffdeeb0aa1c08a43
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2023-12-02 20:00:17 +0100

    Update bash solution

--- 2023/day-02/solve.sh
+++ 2023/day-02/solve.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MAX=$(paste '-d ' \
-  <(cat input.txt | sed -E ':l;s/(.*) ([0-9]+) red(.*)/\2 \1\3/g;tl;s/([0-9 ]+).*/\1/;s/ /\n/g;s/.*/echo "&" | sort -n -r | head -1/;e') \
-  <(cat input.txt | sed -E ':l;s/(.*) ([0-9]+) green(.*)/\2 \1\3/g;tl;s/([0-9 ]+).*/\1/;s/ /\n/g;s/.*/echo "&" | sort -n -r | head -1/;e') \
-  <(cat input.txt | sed -E ':l;s/(.*) ([0-9]+) blue(.*)/\2 \1\3/g;tl;s/([0-9 ]+).*/\1/;s/ /\n/g;s/.*/echo "&" | sort -n -r | head -1/;e') )
+  <(cat input.txt | sed -E ":l;s/(.*)^(.*) ([0-9]+) red(.*)\'/\1\3\n\2 \4/M;tl;s/\n.*\'//M;s/.*/echo \"&\" | sort -nr | head -1/;e") \
+  <(cat input.txt | sed -E ":l;s/(.*)^(.*) ([0-9]+) green(.*)\'/\1\3\n\2 \4/M;tl;s/\n.*\'//M;s/.*/echo \"&\" | sort -nr | head -1/;e") \
+  <(cat input.txt | sed -E ":l;s/(.*)^(.*) ([0-9]+) blue(.*)\'/\1\3\n\2 \4/M;tl;s/\n.*\'//M;s/.*/echo \"&\" | sort -nr | head -1/;e") )
 
 echo "$MAX" | sed -E 's/([0-9]+) ([0-9]+) ([0-9]+)/\1<=12 \&\& \2<=13 \&\& \3<=14/' | bc | sed -n '/1/=' | paste -sd+ | bc
 
